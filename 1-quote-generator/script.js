8bea9efbd14ab8890c3dfd1baf0f2c5fcc31b7fe
Oleg <devsniga@gmail.com>
2021-07-23 01:12:09 +0200

    Lesson 18 is complete. Yu-hu

--- 1-quote-generator/script.js
+++ 1-quote-generator/script.js
@@ -1,11 +1,31 @@
 
+const quoteContainer = document.querySelector('#quote-container')
+const quoteText = document.querySelector('#quote')
+const authorText = document.querySelector('#author')
+const twitterBtn = document.querySelector('#twitter')
+const newQuoteBtn = document.querySelector('#new-quote')
+
+
 let apiQuotes = []
 
 // Show New Quote
 
 let newQuote = () => {
-  let idRandom = Math.floor(Math.random() * apiQuotes.length)
-  return apiQuotes[idRandom]
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+
+  //Check if Author field is blank and replace it with
+  if (!quote.author) {
+    authorText.textContent = 'Unknown'
+  } else {
+    authorText.textContent = quote.author
+  }
+  
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote')
+  } else {
+    quoteText.classList.remove('long-quote')
+  }
+  quoteText.textContent = quote.text
 }
 
 // Get Qoutes From API
@@ -15,12 +35,22 @@ async function getQuotes() {
     const response = await fetch(apiUrl)
     apiQuotes = await response.json()
     
-    console.log(newQuote())
+    newQuote()
   } catch (error) {
 
     // Catch Error Here
   }
 }
+
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+  window.open(twitterUrl, '_blank')
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load
 getQuotes()
